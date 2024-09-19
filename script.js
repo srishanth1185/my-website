29e270cf534b8ca2320336c3191a509cd13225dd
srishanth1185 <srishanth1185@gmail.com>
2024-09-19 17:34:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById("yes-btn").addEventListener("click", function() {
+    alert("Yay! I love you too!");
+});
+
+document.getElementById("no-btn").addEventListener("click", function() {
+    alert("It's ok");
+});
